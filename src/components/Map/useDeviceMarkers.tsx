11566784890c75111681aee656383b4483a03220
Maur025 <mmoyapalli@gmail.com>
2025-05-19 17:45:41 -0400

    feat: enhance useDeviceMarkers to track device movement with line history

--- src/components/Map/useDeviceMarkers.tsx
+++ src/components/Map/useDeviceMarkers.tsx
@@ -2,14 +2,15 @@ import { Feature } from "ol";
 import VectorSource from "ol/source/Vector";
 import { useEffect, useRef, type RefObject } from "react";
 import type { DeviceCurrentLocation } from "../../models/device-current-location";
-import { Geometry, Point } from "ol/geom";
+import { Geometry, LineString, Point } from "ol/geom";
 import { fromLonLat } from "ol/proj";
-import { Icon, Style } from "ol/style";
+import { Icon, Stroke, Style } from "ol/style";
 import { SocketTopic } from "../../socket-topic";
 import { useSocketStore } from "../../store/useSocketStore";
 import { SocketRoom } from "../../socket-room";
 import type { SingleIoResponse } from "@maur025/core-model-data";
 import { useSocketRoomHandler } from "../../hooks/useSocketRoomHandler";
+import type { Coordinate } from "ol/coordinate";
 
 interface DeviceMarkersResponse {
 	deviceFeatureRef: RefObject<Map<string, Feature>>;
@@ -20,6 +21,9 @@ const { MONITOR_ALL_DEVICES } = SocketRoom;
 
 export const useDeviceMarkers = (deviceVectorSource: VectorSource): DeviceMarkersResponse => {
 	const deviceFeatureRef = useRef<Map<string, Feature>>(new Map());
+	const lineMovementRef = useRef<Feature<LineString>>(null);
+	const movementHistoryRef = useRef<Coordinate[]>([]);
+
 	const { socket } = useSocketStore();
 	const { joinRoom, leaveRoom } = useSocketRoomHandler();
 
@@ -40,23 +44,41 @@ export const useDeviceMarkers = (deviceVectorSource: VectorSource): DeviceMarker
 				last: { lat = 0, lon = 0 },
 			} = data;
 
+			const coords: Coordinate = fromLonLat([lon, lat]);
 			let feature = deviceFeatureRef.current.get(deviceId);
 
 			if (!feature) {
 				feature = new Feature({
-					geometry: new Point(fromLonLat([lon, lat])),
+					geometry: new Point(coords),
 				});
 
+				lineMovementRef.current = new Feature(new LineString([]));
+
 				feature.setStyle(new Style({ image: new Icon({ src: "/navigation-3.webp" }) }));
+				lineMovementRef.current.setStyle(
+					new Style({
+						stroke: new Stroke({
+							color: "black",
+							width: 5,
+							lineDash: [5, 15],
+						}),
+					}),
+				);
 
 				deviceVectorSource.addFeature(feature);
+				deviceVectorSource.addFeature(lineMovementRef.current);
+
 				deviceFeatureRef.current.set(deviceId, feature);
 			} else {
 				const geometry: Geometry | undefined = feature.getGeometry();
 
 				if (geometry instanceof Point) {
-					geometry.setCoordinates(fromLonLat([lon, lat]));
+					geometry.setCoordinates(coords);
 				}
+
+				movementHistoryRef.current = [...movementHistoryRef.current, coords];
+
+				lineMovementRef.current?.getGeometry()?.setCoordinates(movementHistoryRef.current);
 			}
 		};
 
